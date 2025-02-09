cdbc72b805cddfc845835d282caa39a20512ff84
sowndv02 <daoson03112002@gmail.com>
2025-02-09 21:38:48 +0700

    [learning] feat: Create project identity server and configuration identity server

---
+++ SecureMicroservices/IdentityServer/Program.cs
@@ -0,0 +1,18 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddTestUsers(new List<TestUser>())
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+app.UseIdentityServer();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
