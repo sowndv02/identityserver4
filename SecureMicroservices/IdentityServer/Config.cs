3f5437e1308a4d4077391eed8220428aa3985f3b
sowndv02 <daoson03112002@gmail.com>
2025-02-10 22:43:43 +0700

    [learning] feat: OpenId connect with IdentityServer 4 interactive MVC Movie.Client

--- SecureMicroservices/IdentityServer/Config.cs
+++ SecureMicroservices/IdentityServer/Config.cs
@@ -1,5 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -18,6 +21,30 @@ namespace IdentityServer
                     }, 
                     AllowedScopes ={ "movieAPI" }
                 }
+                new Client
+                {
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/sigin-oidc" // This is client app port
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>()
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
             };
 
         public static IEnumerable<ApiScope> ApiScopes => 
@@ -26,11 +53,33 @@ namespace IdentityServer
                 new ApiScope("movieAPI", "Movie API")
             };
 
-        public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
+        public static IEnumerable<ApiResource> ApiResources => 
+            new ApiResource[] 
+            {
+            };
 
-        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
+        public static IEnumerable<IdentityResource> IdentityResources => 
+            new IdentityResource[] 
+                {
+                    new IdentityResources.OpenId(),
+                    new IdentityResources.Profile()
+                };
 
-        public static List<TestUser> TestUsers => new List<TestUser> { };   
+        public static List<TestUser> TestUsers => 
+            new List<TestUser> 
+            {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "swn",
+                    Password = "swn", 
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "swn"),
+                        new Claim(JwtClaimTypes.FamilyName, "van")
+                    }
+                }
+            };   
 
     }
 }
--- SecureMicroservices/IdentityServer/Program.cs
+++ SecureMicroservices/IdentityServer/Program.cs
@@ -1,16 +1,20 @@
 using IdentityServer;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllersWithViews();
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
     .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 app.UseIdentityServer();
-app.MapGet("/", () => "Hello World!");
-
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
+app.UseStaticFiles();
 app.Run();
