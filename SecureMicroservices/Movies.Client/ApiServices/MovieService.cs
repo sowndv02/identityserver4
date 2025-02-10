c7de2be6b7ab9e8456ede90546a1d24fed6a1b58
sowndv02 <daoson03112002@gmail.com>
2025-02-10 21:55:25 +0700

    [learning] feat: Create sample Movie.Client MVC

---
+++ SecureMicroservices/Movies.Client/ApiServices/MovieService.cs
@@ -0,0 +1,43 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieService : IMovieService
+    {
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(new Movie() 
+            {
+                Id = 1,
+                Genre = "Comics",
+                Title  = "ads",
+                Rating = "9.2",
+                ImageUrl = "images/src",
+                ReleaseDate = DateTime.Now,
+                Owner = "swn"
+            });
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SecureMicroservices/Movies.Client/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Client.ApiServices;
+using Movies.Client.Models;
+
+namespace Movies.Client.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _movieService.GetMovies());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+           
+            return View();
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return true;
+        }
+    }
+}
